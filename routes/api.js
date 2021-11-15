b1bca8ef40a709a782bb5208b370423eeca0a88e
Ezhko <ucsd.camp.21@gmail.com>
2021-11-15 14:57:29 -0800

    fix connection

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const Workout = require('../models/Workout.js');
+const router = require("express").Router();
+const Workout = require("../models/Workout.js");
 
-router.post('/api/workouts', ({ body }, res) => {
-  Workout.create(body)
+router.post("/api/workouts", (req, res) => {
+  Workout.create(req)
     .then((workout) => {
       res.json(workout);
     })
@@ -11,10 +11,30 @@ router.post('/api/workouts', ({ body }, res) => {
     });
 });
 
-router.get('/api/workouts', (req, res) => {
-  Workout.find(lastWorkout._id)
+router.put(`/api/workouts/:id`, (req, res) => {
+  Workout.updateOne(req.params.id)
     .then((workout) => {
       res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/exercise", (req, res) => {
+  res
+    .sendFile("exercise.html", {
+      root: path.join(__dirname, "./"),
     })
     .catch((err) => {
       res.status(400).json(err);
