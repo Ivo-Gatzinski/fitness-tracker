bcbab2209a62593408b69548b8b353c9bb796cb2
Ezhko <ucsd.camp.21@gmail.com>
2021-11-15 21:37:33 -0800

    update put route

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,11 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-  Workout.updateOne(req.params)
+  console.log(req.body);
+  Workout.updateOne(
+    { _id: `ObjectId("${req.params.id}")` },
+    { $push: { exercises: req.body }}
+  )
     .then((workout) => {
       res.json(workout);
     })
@@ -48,6 +52,6 @@ router.get("/api/workouts/range", (req, res) => {
     .catch((err) => {
       res.status(400).json(err);
     });
-})
+});
 
 module.exports = router;
