bb734867398e305489da6f894e29878902a1bf59
Ezhko <ucsd.camp.21@gmail.com>
2021-11-15 20:42:10 -0800

    try routes

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 const path = require("path");
 
-router.post("/api/workouts", (req, res) => {
-  Workout.create(req)
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
     .then((workout) => {
       res.json(workout);
     })
@@ -12,8 +12,8 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
-router.put(`/api/workouts/:id`, (req, res) => {
-  Workout.updateOne(req.params.id)
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.updateOne(req.params)
     .then((workout) => {
       res.json(workout);
     })
@@ -35,5 +35,19 @@ router.get("/api/workouts", (req, res) => {
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+})
 
 module.exports = router;
